5db0ab470d01ef937da416788f544fe7aefdf902
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-11-26 21:03:23 +0100

    user profile component is added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,45 @@
+import Profile from './Profile/Profile';
+// import Statistics from './Statistics/Statistics';
+// import FriendList from './FriendList/FriendList';
+// import TransactionHistory from './TransactionHistory/TransactionHistory';
 
+import user from '../inputs/user.json';
+
+import PropTypes from 'prop-types';
+
+const App = () => {
+  const { username, tag, location, avatar, stats } = user;
+  return (
+    <>
+      <Profile
+        key={username}
+        name={username}
+        tag={tag}
+        location={location}
+        image={avatar}
+        followers={stats.followers}
+        views={stats.views}
+        likes={stats.likes}
+      />
+      {/* <Statistics />
+      <FriendList />
+      <TransactionHistory /> */}
+    </>
+  );
+};
+
+Profile.propTypes = {
+  user: PropTypes.exact({
+    name: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    stats: PropTypes.exact({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }).isRequired,
+  }),
+};
+
+export default App;

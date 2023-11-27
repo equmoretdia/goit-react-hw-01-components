8a1d804759f64a6080f169c634df5aaf8417f829
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-11-27 09:52:28 +0100

    app and user profile component refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,41 +8,15 @@ import data from '../data/data.json';
 import friends from '../data/friends.json';
 import transactions from '../data/transactions.json';
 
-import PropTypes from 'prop-types';
-
 const App = () => {
-  const { username, tag, location, avatar, stats } = user;
   return (
     <>
-      <Profile
-        key={username}
-        name={username}
-        tag={tag}
-        location={location}
-        image={avatar}
-        followers={stats.followers}
-        views={stats.views}
-        likes={stats.likes}
-      />
+      <Profile user={user} />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory transactions={transactions} />
     </>
   );
-};
-
-Profile.propTypes = {
-  user: PropTypes.exact({
-    name: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    stats: PropTypes.exact({
-      followers: PropTypes.number.isRequired,
-      views: PropTypes.number.isRequired,
-      likes: PropTypes.number.isRequired,
-    }).isRequired,
-  }),
 };
 
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,13 @@
 import css from './Profile.module.css';
 import PropTypes from 'prop-types';
 
-const Profile = ({ name, tag, location, image, followers, views, likes }) => {
+const Profile = ({ user }) => {
+  const { username, tag, location, avatar, stats } = user;
   return (
-    <div className={css.profile}>
+    <div className={css.profile} key={username}>
       <div className={css.description}>
-        <img src={image} alt="User avatar" className={css.avatar} />
-        <p className={css.name}>{name}</p>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
         <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
       </div>
@@ -14,15 +15,15 @@ const Profile = ({ name, tag, location, image, followers, views, likes }) => {
       <ul className={css.stats}>
         <li>
           <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{followers}</span>
+          <span className={css.quantity}>{stats.followers}</span>
         </li>
         <li>
           <span className={css.label}>Views</span>
-          <span className={css.quantity}>{views}</span>
+          <span className={css.quantity}>{stats.views}</span>
         </li>
         <li>
           <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{likes}</span>
+          <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
@@ -30,13 +31,17 @@ const Profile = ({ name, tag, location, image, followers, views, likes }) => {
 };
 
 Profile.propTypes = {
-  name: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  followers: PropTypes.number.isRequired,
-  views: PropTypes.number.isRequired,
-  likes: PropTypes.number.isRequired,
+  user: PropTypes.exact({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.exact({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }).isRequired,
+  }),
 };
 
 export default Profile;

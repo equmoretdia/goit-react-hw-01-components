a20f97f0dc339cdb9b9b4fc52c7c01bc1c653caf
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-11-28 09:45:25 +0100

    friend list item component is separated from friend list component in accordance to tech requirements

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,4 +1,5 @@
 import css from './FriendList.module.css';
+import FriendListItem from '../FriendListItem/FriendListItem';
 import PropTypes from 'prop-types';
 
 const FriendsList = ({ friends }) => {
@@ -6,18 +7,12 @@ const FriendsList = ({ friends }) => {
     <ul className={css.list}>
       {friends.map(({ avatar, name, isOnline, id }) => {
         return (
-          <li className={css.item} key={id}>
-            <span
-              className={`${css.status} ${isOnline ? css.online : css.offline}`}
-            ></span>
-            <img
-              className={css.avatar}
-              src={avatar}
-              alt="User avatar"
-              width="48"
-            />
-            <p className={css.name}>{name}</p>
-          </li>
+          <FriendListItem
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+            key={id}
+          />
         );
       })}
     </ul>
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,22 @@
+import css from './FriendListItem.module.css';
+import PropTypes from 'prop-types';
+
+const FriendsListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={css.item}>
+      <span
+        className={`${css.status} ${isOnline ? css.online : css.offline}`}
+      ></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
+
+FriendsListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+export default FriendsListItem;
